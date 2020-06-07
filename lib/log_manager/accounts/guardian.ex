a58c6c3b5dc13539004c14d61e2040f5d5db203c
Patrick Jones <patrickjones.pmj@gmail.com>
2020-06-06 23:07:27 -0500

    cleanup

--- lib/log_manager/accounts/guardian.ex
+++ lib/log_manager/accounts/guardian.ex
@@ -4,38 +4,14 @@ defmodule LogManager.Guardian do
 
   alias LogManager.Accounts
 
-  # def subject_for_token(user, _claims) do
-  #   {:ok, to_string(user.id)}
-  # end
-
-  # # def subject_for_token(_, _) do
-  # #   {:error, :reason_for_error}
-  # # end
-
-  # def resource_from_claims(claims) do
-  #   # def resource_from_claims(%{"sub" => id}) do
-  #   id = claims["sub"]
-  #   user = Accounts.get_user!(id)
-  #   {:ok, user}
-  # rescue
-  #   Ecto.NoResultsError -> { :error, :resource_not_found }
-  # end
   def subject_for_token(user, _claims) do
     sub = to_string(user.id)
     {:ok, sub}
-  end
-
-  def subject_for_token(_, _) do
-    {:error, :reason_for_error}
   end
 
   def resource_from_claims(claims) do
     id = claims["sub"]
     resource = Accounts.get_user!(id)
     {:ok,  resource}
-  end
-
-  def resource_from_claims(_claims) do
-    {:error, :reason_for_error}
   end
 end
