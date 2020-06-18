54cf77d5f04d4e12e0deb0cd60491faa6b6431db
Patrick Jones <patrickjones.pmj@gmail.com>
2020-06-17 23:18:20 -0500

    update project_channel; import Channel Helpers in LogManagerWeb entrypoint

--- lib/log_manager_web/channels/project_channel.ex
+++ lib/log_manager_web/channels/project_channel.ex
@@ -1,6 +1,5 @@
 defmodule LogManagerWeb.ProjectChannel do
-  # use LogManagerWeb, :channel
-  use Phoenix.Channel
+  use LogManagerWeb, :channel
 
   @impl true
   def join("project:" <> projectId, _payload, socket) do
@@ -21,25 +20,6 @@ defmodule LogManagerWeb.ProjectChannel do
   def handle_in("shout", payload, socket) do
     broadcast socket, "shout", payload
     {:noreply, socket}
-  end
-
-  # Check whether the user is allowed to listen to this resource
-  defp authorized?(socket, id) do
-    user = get_current_user(socket)
-    project = LogManager.Projects.get_project!(id)
-    # IO.puts("User: " <> inspect(user))
-    # IO.puts("ProjectUser: " <> inspect(project))
-    if (project && project.user.id === user.id) do
-      true
-    else
-      false
-    end
-    # IO.puts(inspect(get_current_user(socket).name))
-    # true
-  end
-
-  defp get_current_user(socket) do
-    socket.assigns |> Map.get(:current_user)
   end
 end
 
